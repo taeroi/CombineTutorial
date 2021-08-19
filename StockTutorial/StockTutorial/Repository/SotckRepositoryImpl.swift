67c62162875392c1cc2194415b96c293fce27b1e
taeroi <fkrkdlsgk@gmail.com>
2021-08-20 00:02:27 +0900

    feat: 레포지토리 생성 & API 키 지정

---
+++ StockTutorial/StockTutorial/Repository/SotckRepositoryImpl.swift
@@ -0,0 +1,14 @@
+//
+//  SotckRepositoryImpl.swift
+//  StockTutorial
+//
+//  Created by 태로고침 on 2021/08/19.
+//
+
+
+/// 구현체
+class SotckRepositoryImpl: StockRepository {
+    var apiKey: String = "11LMTU6DRYEESBO0"
+    
+    
+}
---
+++ StockTutorial/StockTutorial/Repository/StockRepository.swift
@@ -0,0 +1,12 @@
+//
+//  StockRepository.swift
+//  StockTutorial
+//
+//  Created by 태로고침 on 2021/08/19.
+//
+
+import Foundation
+
+protocol StockRepository {
+    var apiKey: String { get set }
+}
