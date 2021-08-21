a828a3ce43496b98dcd615581663499a1d21865c
taeroi <fkrkdlsgk@gmail.com>
2021-08-22 01:20:48 +0900

    feat: BaseViewController 추가 + 수정
    1. 키보드 노티 추가
    2. Subscriber 추가

--- StockTutorial/StockTutorial/Controller/BaseViewController.swift
+++ StockTutorial/StockTutorial/Controller/BaseViewController.swift
@@ -12,7 +12,8 @@ import Combine
 class BaseViewController: UIViewController {
     
     let disposeBag = DisposeBag()
-    var subscriber: Set<AnyCancellable> = .init() 
+    var subscriber: Set<AnyCancellable> = .init()
+    private var scrollView: UIScrollView?
     
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
@@ -22,13 +23,61 @@ class BaseViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    
+    //MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupViews()
+        setupTableView()
     }
+    
+    
+    //MARK: - Methods
     
     func setupViews() {
         view.backgroundColor = .systemBackground
+    }
+    
+    func setupTableView() { }
+    
+    
+    // ----- Keyboard Observer -----
+    
+    func connectObservers(scrollView: UIScrollView) {
+        self.scrollView = scrollView
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillShow),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillHide),
+                                               name: UIResponder.keyboardDidHideNotification,
+                                               object: nil)
+    }
+    
+    func disconnectObservers() {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    @objc
+    private func keyboardWillShow(notification: NSNotification) {
+        guard let scrollView = scrollView,
+              let userInfo = notification.userInfo else { return }
+        var keyboradFrame: CGRect = (userInfo[UIResponder.keyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+        keyboradFrame = self.view.convert(keyboradFrame, from: nil)
+        
+        var contentInset: UIEdgeInsets = scrollView.contentInset
+        contentInset.bottom = keyboradFrame.size.height + 20
+        scrollView.contentInset = contentInset
+    }
+    
+    @objc
+    private func keyboardWillHide(notification: NSNotification) {
+        guard let scrollView = scrollView else { return }
+        
+        let contentInset: UIEdgeInsets = .zero
+        scrollView.contentInset = contentInset
     }
 }
