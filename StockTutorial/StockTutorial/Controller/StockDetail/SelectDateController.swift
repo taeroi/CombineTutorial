6f0d5e31f65f478df72beb0cea4db65d21227bca
taeroi <fkrkdlsgk@gmail.com>
2021-08-22 18:39:18 +0900

    feat: SelectDateController 추가

---
+++ StockTutorial/StockTutorial/Controller/StockDetail/SelectDateController.swift
@@ -0,0 +1,15 @@
+//
+//  SelectDateController.swift
+//  StockTutorial
+//
+//  Created by 박태환 on 2021/08/22.
+//
+
+import UIKit
+
+class SelectDateController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
