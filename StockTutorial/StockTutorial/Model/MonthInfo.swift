cb36d514df3843c3935b398266d8b2f6ee477b42
taeroi <fkrkdlsgk@gmail.com>
2021-08-22 18:01:07 +0900

    feat: MainInfo 모델 추가

---
+++ StockTutorial/StockTutorial/Model/MonthInfo.swift
@@ -0,0 +1,14 @@
+//
+//  MonthInfo.swift
+//  StockTutorial
+//
+//  Created by 박태환 on 2021/08/22.
+//
+
+import Foundation
+
+struct MonthInfo {
+    var date: Date
+    var adjustedOpen: Double
+    var adjustedClose: Double
+}
