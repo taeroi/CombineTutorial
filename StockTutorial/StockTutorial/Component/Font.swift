9cb934da528d7365270d90d6c7d0e43dd30e70cc
taeroi <fkrkdlsgk@gmail.com>
2021-08-22 15:37:25 +0900

    feat: component 추가

--- StockTutorial/StockTutorial/Component/Font.swift
+++ StockTutorial/StockTutorial/Component/Font.swift
@@ -7,6 +7,37 @@
 
 import UIKit
 
+
+// MARK: - Small Label
+
+class SmallLabel: UILabel {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        font = UIFont.systemFont(ofSize: 12)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+class SmallBoldLabel: UILabel {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        font = UIFont.boldSystemFont(ofSize: 12)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+ 
+
+//MARK: - Color Label
+
 class NormalGrayLabel: UILabel {
     
     override init(frame: CGRect) {
@@ -19,6 +50,8 @@ class NormalGrayLabel: UILabel {
     }
 }
 
+
+//MARK: Normal Label
 
 class NormalLabel: UILabel {
     
--- StockTutorial/StockTutorial/Component/View.swift
+++ StockTutorial/StockTutorial/Component/View.swift
@@ -8,9 +8,19 @@
 import UIKit
 
 extension UIView {
+    
+    /// 하위 뷰를 추가합니다
     func addSubviews(views: [UIView]) {
         views.forEach { someView in
             self.addSubview(someView)
         }
     }
+    
+    ///지정된 View들의 TranslatesAutoresizingMaskIntoConstraints를 false로 설정합니다
+    func addTranslatesAutoresizingMaskIntoConstraints(views: [UIView]) {
+        views.forEach { someView in
+            someView.translatesAutoresizingMaskIntoConstraints = false
+        }
+    }
+    
 }
