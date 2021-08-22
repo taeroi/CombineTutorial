fb902b65e2bf2100ceebc2373488b5535ca62e14
taeroi <fkrkdlsgk@gmail.com>
2021-08-22 18:02:55 +0900

    refactor: TimeSeriesMontlyAdjusted 모델 안에 monthInfos 모델 삽입(상태관리 용이하게)

--- StockTutorial/StockTutorial/Model/TimeSeriesMontlyAdjusted.swift
+++ StockTutorial/StockTutorial/Model/TimeSeriesMontlyAdjusted.swift
@@ -10,6 +10,7 @@ import Foundation
 struct TimeSeriesMontlyAdjusted: Decodable {
     let meta: Meta
     let series: [String: OHLC]
+    var monthInfos: [MonthInfo] = []
     
     enum CodingKeys: String, CodingKey {
         case meta = "Meta Data"
@@ -34,5 +35,36 @@ struct TimeSeriesMontlyAdjusted: Decodable {
             case close = "4. close"
             case adjustedClose = "5. adjusted close"
         }
+    }
+    
+    mutating func generateMonthInfos() {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        
+        var monthInfos: [MonthInfo] = []
+        
+        let sortedSeries = series.sorted { $0.key > $1.key }
+        sortedSeries.forEach { (dateString, ohlc) in
+            if let date = dateFormatter.date(from: dateString),
+               let adjustedOpen = generateAdjustedOpen(ohlc: ohlc),
+               let adjustedClose = Double(ohlc.adjustedClose) {
+                let monthInfo: MonthInfo = .init(date: date,
+                                                  adjustedOpen: adjustedOpen,
+                                                  adjustedClose: adjustedClose)
+                monthInfos.append(monthInfo)
+            }
+        }
+        self.monthInfos = monthInfos
+    }
+    
+    private func generateAdjustedOpen(ohlc: OHLC) -> Double? {
+        var adjustedOpen: Double?
+        
+        if let open = Double(ohlc.open),
+           let close = Double(ohlc.close),
+           let adjustedClose = Double(ohlc.adjustedClose) {
+            adjustedOpen = open * adjustedClose / close
+        }
+        return adjustedOpen
     }
 }
