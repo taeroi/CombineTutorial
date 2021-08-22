4a3cfbe47f8f43c8869b9d9c54774bebf76fad4a
taeroi <fkrkdlsgk@gmail.com>
2021-08-22 10:15:33 +0900

    refactor: url error case - combine 예외처리 추가

--- StockTutorial/StockTutorial/Repository/SotckRepositoryImpl.swift
+++ StockTutorial/StockTutorial/Repository/SotckRepositoryImpl.swift
@@ -16,7 +16,10 @@ class SotckRepositoryImpl: StockRepository {
     func fetchStocksPublisher(keywords: String) -> AnyPublisher<StockResult, Error> {
         let urlString = "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=\(keywords)&apikey=\(apiKey)"
         
-        let url = URL(string: urlString)!
+        guard let url = URL(string: urlString) else {
+            let error = URLError(.badURL)
+            return Fail(error: error).eraseToAnyPublisher()
+        }
         
         return URLSession.shared.dataTaskPublisher(for: url)
             .map{ $0.data }
